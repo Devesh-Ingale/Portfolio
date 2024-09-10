e7425a9ae5a9d66e44c749225824bd80ed983d1a
Devesh-Ingale <150885752+Devesh-Ingale@users.noreply.github.com>
2024-09-11 00:00:13 +0530

    projectd

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import aboutImg from "../assets/profilepic.png";
 import book from "../assets/book.png";
 import pc from "../assets/pc.png";
 import card from "../assets/card.png";
 import finance from "../assets/finance.png";
-import { Span } from "next/dist/trace";
 
 const About = ()=>{
     return (
